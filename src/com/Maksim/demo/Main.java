a7d9b0a933f2c456495e5d18c561ec93325cd44d
Maksim <Mp-104@hotmail.com>
2023-09-27 14:10:27 +0200

    prototypeDiceGame

--- src/com/Maksim/demo/Main.java
+++ src/com/Maksim/demo/Main.java
@@ -14,22 +14,37 @@ public class Main {
         // instantiate
         Player player1 = new Player();
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
 
         //Variables
         ArrayList<Player> playerlist = new ArrayList<>();
 
+        System.out.println("how many players?");
+        int players = scanner.nextInt();
+        System.out.println(players + " players");
+
+        System.out.println("how many rounds?");
+        int rounds = scanner.nextInt();
+        System.out.println(rounds + " rounds");
+
+        System.out.println("how many dices");
+        int dices = scanner.nextInt();
+        System.out.println(dices + " dices");
+
+        scanner.nextLine();   // player1.name = scanner.nextLine(); below was skipped, for some reason, this line prevents that and is skipped instead
 
         System.out.println("what is your name?");
         player1.name = scanner.nextLine();
         System.out.println("Your name is: " + player1.name);
 
 
-        for (int i = 0; i < 5; i++) {   // ersätt i < 5 med user input som efterfrågar antal spelare.
+        for (int i = 0; i < players; i++) {   // ersätt i < 5 med user input som efterfrågar antal spelare.
 
             //create Unique players
+            System.out.println("Input player " + (i+1 )+ " name"); // +1 to better represent amount of players
             Player player = new Player ();
-            player.name = "player " + scanner.nextLine();  // ersätt i med scanner för att få unikt namn scanner.nextLine()
+            player.name = "player "+ (i+1)+" " + scanner.nextLine();  // i+1 so it doesn't start at 0, more humanely represents amount of players
 
             System.out.println(player.name);
             playerlist.add(player);
@@ -42,6 +57,28 @@ public class Main {
         for (int i = 0; i < playerlist.size(); i++) {
 
             System.out.println(playerlist.get(i).name);
+
+        }
+
+        for (int i = 0; i < rounds; i++) {
+            System.out.println("round " + (i+1) );
+
+            for (int j = 0; j < playerlist.size(); j++) {
+                System.out.println(playerlist.get(j).name);
+
+
+                for (int k = 0; k < dices; k++) {
+                    int dice = random.nextInt(1,6);
+                    System.out.println("dice " + (k+1) + " " + dice);
+
+                    playerlist.get(j).score += dice;
+                    System.out.println(playerlist.get(j).name + " " + playerlist.get(j).score);
+
+                }
+
+
+
+            }
 
         }
 
