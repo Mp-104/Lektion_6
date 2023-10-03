0240b8409e50cc06e32646bd9044a3ca93cc536b
Maksim <Mp-104@hotmail.com>
2023-10-03 16:00:04 +0200

    betaDice

--- src/com/Maksim/demo/Main.java
+++ src/com/Maksim/demo/Main.java
@@ -23,79 +23,79 @@ public class Main {
 
 
 
-        System.out.println("Welcome to dice! How many players? Insert number");
+        System.out.println("Välkommen till tärning! Hur många spelar? Ange ett heltal");
         while (!scanner.hasNextInt()) {                         // while loopen fortsätter köra scanner tills den får integer
-            System.out.println("That's not a number!");
+            System.out.println("Det är inte ett heltal!");
             scanner.next();
         }
         int players = scanner.nextInt();
-        System.out.println(players + " Players");
+        System.out.println(players + " Spelare");
 
 
-        System.out.println("How many rounds? Insert number");
+        System.out.println("Hur många rundor? Ange ett heltal");
         while (!scanner.hasNextInt()) {
-            System.out.println("That's not a number!");
+            System.out.println("Det är inte ett heltal!");
             scanner.next();
         }
         int rounds = scanner.nextInt();
-        System.out.println(rounds + " Rounds");
+        System.out.println(rounds + " Rundor");
 
 
-        System.out.println("How many dices");
+        System.out.println("Hur många tärningar? Ange ett heltal!");
         while (!scanner.hasNextInt()) {
-            System.out.println("That's not a number!");
+            System.out.println("Det är inte ett heltal");
             scanner.next();
         }
         int dices = scanner.nextInt();
-        System.out.println(dices + " Dices");
+        System.out.println(dices + " Tärningar");
 
         scanner.nextLine();   // player1.name = scanner.nextLine(); below was skipped, for some reason, this line prevents that and is skipped instead
 
-        System.out.println("what is your name?");
+        /*System.out.println("what is your name?");
         player1.name = scanner.nextLine();
-        System.out.println("Your name is: " + player1.name);
+        System.out.println("Your name is: " + player1.name); */
 
 
-        for (int i = 0; i < players; i++) {   // ersätt i < 5 med user input som efterfrågar antal spelare.
+        for (int i = 0; i < players; i++) {
 
             //create Unique players
-            System.out.println("Input player " + (i+1 )+ " name"); // +1 to better represent amount of players
+            System.out.println("Ange namn för spelare " + (i+1 )); // +1 to better represent amount of players
             Player player = new Player ();
-            player.name = /*"player "+ (i+1)+": "*/ scanner.nextLine();  // i+1 so it doesn't start at 0, more humanely represents amount of players
+            player.name = scanner.nextLine();
 
-            System.out.println("Player " + (i+1) + ": " + player.name);
+            System.out.println("Spelare " + (i+1) + ": " + player.name);
             playerlist.add(player);
 
-            System.out.println("Playerlist " + playerlist);
+            // System.out.println("Playerlist " + playerlist);
 
         }
 
-        System.out.println("playerlist: " + playerlist);  // skriver ut address och inte själva ArrayList !!! @Override toString
+        //System.out.println("playerlist: " + playerlist);  // skriver ut address och inte själva ArrayList !!! @Override toString
 
 
-        System.out.println("Participating players: ");
+        System.out.println("Medverkande spelare: ");
         for (int i = 0; i < playerlist.size(); i++) {
 
             System.out.println(playerlist.get(i).name);
 
         }
 
-        for (int i = 0; i < rounds; i++) {
-            System.out.println("round " + (i+1) );
+        for (int i = 0; i < rounds; i++) {   // rounds är en variabel som spelare matar in med en int, loopen körs så många ggr
+            System.out.println("Runda " + (i+1) );
 
             for (int j = 0; j < playerlist.size(); j++) {
-                System.out.println(playerlist.get(j).name + "'s turn");
+                System.out.println(playerlist.get(j).name + "'s tur");
 
 
                 for (int k = 0; k < dices; k++) {
-                    int dice = random.nextInt(1,7);  // får aldrig mer än 5.. öka bound till 7 ?
-                    System.out.println("Dice " + (k+1) + ": " + dice);
+                    int dice = random.nextInt(1,7);   // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! TEST
+                    System.out.println("Tärning " + (k+1) + ": " + dice);
 
                     playerlist.get(j).score += dice;
 
 
                 }
-                System.out.println(playerlist.get(j).name + " has " + playerlist.get(j).score + " points!") ;
+                System.out.println(playerlist.get(j).name + " har " + playerlist.get(j).score + " poäng!") ;
 
 
 
@@ -104,27 +104,45 @@ public class Main {
 
 
         }
-        int temp = 0;
+        // var temp = 0;
 
-        for (int i = 0; i < playerlist.size(); i++) {
-            System.out.println("playerlist.get(i).name: " + playerlist.get(i).name + "playerlist.get(i).score" + playerlist.get(i).score);
+        /*for (int i = 0; i < playerlist.size(); i++) {
+            System.out.println("playerlist.get(i).name: " + playerlist.get(i).name + " playerlist.get(i).score: " + playerlist.get(i).score);
             System.out.println(playerlist.get(i).score);
-            System.out.println(playerlist);
+            System.out.println(playerlist); */
 
-            /*for (int j = i+1 ; j < playerlist.size(); j++) {
-                if (playerlist.get(i).score > playerlist.get(j).score); {
+
+            /* for (int j = i+1 ; j < playerlist.size(); j++) {
+                if (playerlist.get(i).score < playerlist.get(j).score); {
                     temp = playerlist.get(i).score;
-                    playerlist.get(i).score = playerlist.get(j).score;
+                    playerlist.get(i).score = playerlist.get(j).score; // omfördelar score mellan spelaren, sorterar inte listan efter
                     playerlist.get(j).score = temp;
 
 
                 }
 
-            } */
+            }
+
+        } */
+
+
+
+        System.out.println("Slutresultatet: ");
+        Player winner = playerlist.get(0);
+        for (Player player : playerlist) {
+            System.out.println(player.name + " fick " + player.score + " poäng");
+            if (player.score > winner.score) {
+                winner = player;
+
+
+            }
 
 
 
         }
+        System.out.println("Du vann, " + winner.name + "!");
+
+
 
 
     }
@@ -144,3 +162,9 @@ for i in range(0, len(arr)):
 print(arr)
 
  */
+/*Player winner = playerlist.get(0);
+   for (Player player : playerlist) {
+      if (player.getScore () > winner.getscore())  {
+      winner = player
+
+   } */
--- src/com/Maksim/demo/Player.java
+++ src/com/Maksim/demo/Player.java
@@ -13,3 +13,4 @@ public class Player {
     int score;
 
 }
+
